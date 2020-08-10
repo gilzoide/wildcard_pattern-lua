1bdd891027d06faec19e99d9ab44db6820ca7d43
gilzoide <gilzoide@gmail.com>
2020-08-10 12:54:58 -0300

    Fix '\' escapes

--- wildcard_pattern.lua
+++ wildcard_pattern.lua
@@ -1,10 +1,28 @@
 local wildcard_pattern = {}
 
-local function sub_star(s)
-    if #s == 1 then
+local function sub_question_mark(prefix, s)
+    if prefix == '\\' then
+        return '%?'
+    else
+        return '[^/]'
+    end
+end
+local function sub_star(prefix, s)
+    if prefix == '\\' then
+        return '%*' .. sub_star('', s:sub(2))
+    elseif #s == 0 then
+        return ''
+    elseif #s == 1 then
         return "[^/]*"
     else
         return ".*"
+    end
+end
+local function sub_backslash(s)
+    if s:match('%w') then
+        return s
+    else
+        return '%' .. s
     end
 end
 --- Create a Lua pattern from wildcard.
@@ -25,8 +43,10 @@ end
 --   '[!' -> '[^'
 function wildcard_pattern.from_wildcard(s, unanchored)
     s = s:gsub('[%%%.%(%)+-]', '%%%0')
-    s = s:gsub('\\(.)', '%%%1')
-    s = s:gsub('%?', '[^/]'):gsub('%*+', sub_star):gsub('%[!', '[%^')
+    s = s:gsub('([^?]?)%?', sub_question_mark)
+        :gsub('([^*]?)(%*+)', sub_star)
+        :gsub('%[!', '[%^')
+        :gsub('\\(.)', sub_backslash)
     if not unanchored then
         s = '^' .. s .. '$'
     end
