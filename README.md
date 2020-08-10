bbcacd25f0b77b8b74c33c9140b7543e134dcead
gilzoide <gilzoide@gmail.com>
2020-08-10 17:29:17 -0300

    WIP read gitignore-like files

--- README.md
+++ README.md
@@ -1,6 +1,18 @@
-# wildcard-pattern
+# wildcard\_pattern.lua
 Lua library for using shell-like wildcards as string patterns.
-Supports importing wildcards from gitignore-like files.
+
+Supports importing wildcards from gitignore-like content.
+
+## What is supported
+- A single asterisk `*` matches zero or more characters that are not directory separators `/`
+- Two consecutive asterisks `**` match zero or more characters
+- A question mark `?` match any character that is not a directory separator `/`
+- Brackets `[...]` denote character sets and ranges, like `[abcd]` and `[a-d]`
+- Brackets may be negated with an exclamation mark `[!...]`
+- Backslash `\` escapes are maintained
+
+## What is not supported
+- Prefix exclamation mark `!` for negating the pattern in ignore files
 
 ## Running tests
 Run tests using [busted](https://olivinelabs.com/busted/)
--- wildcard_pattern.lua
+++ wildcard_pattern.lua
@@ -43,23 +43,64 @@ end
 --   '[!' -> '[^'
 --
 -- @tparam string s Wildcard string
--- @param[opt] unanchored If present and truthy, don't anchor pattern in string bounds with '^' and '$'
+-- @param[opt] anchor_to_slash If truthy, anchor pattern to directory separators instead of to the begining of `s`
 --
 -- @treturn string Lua pattern corresponding to given wildcard
-function wildcard_pattern.from_wildcard(s, unanchored)
+function wildcard_pattern.from_wildcard(s, anchor_to_slash)
     s = s:gsub('[%%%.%(%)+-]', '%%%0')
     s = s:gsub('([^?]?)%?', sub_question_mark)
         :gsub('([^*]?)(%*+)', sub_star)
         :gsub('%[!', '[%^')
         :gsub('\\(.)', sub_backslash)
-    if not unanchored then
-        s = '^' .. s .. '$'
-    end
-    return s
+    local anchor = anchor_to_slash and '%f[/]' or '^'
+    return anchor .. s .. '$'
 end
 
-function wildcard_pattern.aggregate(...)
-    
+--- Try matching `s` to every pattern in `t`, returning `s` if any match occurs
+--
+-- @treturn[1] string `s`, if there was a match
+-- @return[2] `false` if `s` didn't match any pattern in `t`
+function wildcard_pattern.any_match(t, s)
+    for i, patt in ipairs(t) do
+        local m = s:match(patt)
+        if m then
+            return s
+        end
+    end
+    return false
+end
+
+--- Create a table with Lua patterns from a gitignore-like content.
+--
+-- @param contents String, line iterator function (e.g., `io.lines(...)`),
+--                 or a table or userdata containing a `lines` method (e.g., files).
+function wildcard_pattern.from_ignore(contents, comment_prefix)
+    local content_type, line_iterator = type(contents)
+    if content_type == 'string' then
+        line_iterator = string.gmatch(contents, "[^\n]*")
+    elseif content_type == 'table' or content_type == 'userdata' then
+        line_iterator = content_type.lines and content_type:lines()
+        if not line_iterator then
+            return nil, string.format("Couldn't find a `lines` method in given %s", content_type)
+        end
+    elseif content_type == 'function' then
+        line_iterator = contents
+    else
+        return nil, string.format("Expected contents be a string, table, userdata or function, found %s", content_type)
+    end
+
+    comment_prefix = comment_prefix or '#'
+    local comment_prefix_length = #comment_prefix
+    local t = {}
+    for line in line_iterator do
+        if line:sub(1, comment_prefix_length) ~= comment_prefix then
+            local trimmed = line:match("^%s*(.-)%s*$")
+            if trimmed ~= '' then
+                table.insert(t, wildcard_pattern.from_wildcard(trimmed, trimmed:find("/", 1, true)))
+            end
+        end
+    end
+    return t
 end
 
 return wildcard_pattern
